9a156a3aa2819dac6f0450c3bef418e08d667af9
ibrar-web <ibrarjut@yahoo.com>
2026-01-06 12:28:10 +0500

    admin stats updated

--- src/v1/modules/admin/stats/stats.service.ts
+++ src/v1/modules/admin/stats/stats.service.ts
@@ -4,7 +4,7 @@ import { Between, Repository } from 'typeorm';
 import { User } from '../../../entities/user.entity';
 import { Supplier } from '../../../entities/supplier.entity';
 import { QuoteRequest } from '../../../entities/quotes/quote-request.entity';
-import { QuoteOffer, QuoteStatus } from '../../../entities/quote-offers.entity';
+import { QuoteOffer } from '../../../entities/quote-offers.entity';
 import { Order } from '../../../entities/quotes/order.entity';
 
 @Injectable()
@@ -36,26 +36,12 @@ export class AdminStatsService {
       where: { createdAt: Between(startOfMonth, now) },
     });
 
-    const revenueMetrics = await this.revenueSummary();
-
     return {
       totalUsers,
       totalSuppliers,
       totalOrders,
       totalEnquiries,
       enquiriesThisMonth,
-      revenue: revenueMetrics,
     };
-  }
-
-  private async revenueSummary() {
-    const acceptedQuotes = await this.quotes.find({
-      where: { status: QuoteStatus.ACCEPTED },
-    });
-    const totalRevenue = acceptedQuotes.reduce(
-      (sum, q) => sum + Number(q.price),
-      0,
-    );
-    return { totalRevenue, acceptedQuotes: acceptedQuotes.length };
   }
 }
